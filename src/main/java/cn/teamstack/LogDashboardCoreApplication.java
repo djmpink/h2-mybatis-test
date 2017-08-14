bf58bfc6be62bd2d75b0fc7c47cb6e161094c7dc
zhouli <343787567@qq.com>
2017-08-14 23:01:10 +0800

    springboot 降级

--- src/main/java/cn/teamstack/LogDashboardCoreApplication.java
+++ src/main/java/cn/teamstack/LogDashboardCoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
 @EnableAdminServer
-@EnableWebSocket
+//@EnableWebSocket
 public class LogDashboardCoreApplication {
 
 	public static void main(String[] args) {
